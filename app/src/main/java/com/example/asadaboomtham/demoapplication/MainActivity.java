f90f7c04c12c9903daf038d02b5c5d7913ed1b5a
asadaboontham <se57160514@gmail.com>
2017-11-16 21:30:00 +0700

    16/11/17 21:28 swipe to refresh complete asada boontham

--- app/src/main/java/com/example/asadaboomtham/demoapplication/MainActivity.java
+++ app/src/main/java/com/example/asadaboomtham/demoapplication/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.asadaboomtham.demoapplication;
 
 import android.app.ProgressDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -22,31 +24,65 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
 
-import static android.R.attr.author;
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final String URL_DATA = " http://c79466f4.ngrok.io/reddit/test.php";
+    private static final String URL_DATA = "http://2a75ac3a.ngrok.io/reddit/test.php";
     private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
-    private List<ListItem> listItems;
+    //private RecyclerView.Adapter adapter;
+    // private List<ListItem> listItems; //mArray
+    private ArrayList<ListItem> mArrayList;
+    private MyAdapter mAdapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swiperefresh);
 
+
+        //mSwipeRefreshLayout = (SwipeRefreshLayout) recyclerView.findViewById(R.id.swiperefresh);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarr);
+        //setSupportActionBar(toolbar);
         recyclerView = (RecyclerView) findViewById(R.id.RecyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        listItems = new ArrayList<>(4);
+        mArrayList = new ArrayList<>(4);
 
-        loadRecyclerViewData();
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swiperefresh);
+        swipeRefreshLayout.setOnRefreshListener(this);
+
+        swipeRefreshLayout.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+
+                                        swipeRefreshLayout.setRefreshing(true);
+                                        if (mArrayList != null) {
+                                            mArrayList.clear();
+                                        }
+                                        loadRecyclerViewData();
+
+                                    }
+                                }
+        );
 
     }
+
+    /**
+     * This method is called when swipe refresh is pulled down
+     */
+    @Override
+    public void onRefresh() {
+        loadRecyclerViewData();
+        if (mArrayList != null)
+
+        {
+            mArrayList.clear();
+        }
+    }
+
 
     private void loadRecyclerViewData() {
         final ProgressDialog progressDialog = new ProgressDialog(this);
@@ -63,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
                             JSONObject jsonObject = new JSONObject(s);
                             JSONArray array = jsonObject.getJSONArray("data");
 
-                            for (int i = 0; i<array.length(); i++) {
+                            for (int i = 0; i < array.length(); i++) {
                                 JSONObject o = array.getJSONObject(i);
                                 ListItem item = new ListItem(
                                         o.getString("news_head"),
@@ -71,30 +107,69 @@ public class MainActivity extends AppCompatActivity {
                                         o.getString("news_ref"),
                                         o.getString("news_pics")
 
-                                           ////edit
+                                        ////edit
 
                                 );
-                                listItems.add(item);
+                                mArrayList.add(item);
                             }
-                            adapter = new MyAdapter(listItems, getApplicationContext());
-                            recyclerView.setAdapter(adapter);
+                            mAdapter = new MyAdapter(mArrayList, getApplicationContext());
+                            recyclerView.setAdapter(mAdapter);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
 
                     }
                 },
+
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError volleyError) {
                         progressDialog.dismiss();
-                        Toast.makeText(getApplicationContext(),volleyError.getMessage(),Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), volleyError.getMessage(), Toast.LENGTH_LONG).show();
+
                     }
                 });
-
+        swipeRefreshLayout.setRefreshing(false);
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
 
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem search = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(search);
+        search(searchView);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void search(SearchView searchView) {
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                if (mAdapter != null) mAdapter.getFilter().filter(newText);
+                return true;
+            }
+        });
+
+    }
 }
+
--- app/src/main/java/com/example/asadaboomtham/demoapplication/MyAdapter.java
+++ app/src/main/java/com/example/asadaboomtham/demoapplication/MyAdapter.java
@@ -5,28 +5,46 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by asada boomtham on 27/9/2560.
  */
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> implements Filterable {
 
-    private List<ListItem> listItems;
+    //private List<ListItem> listItems;
     private Context context;
+    //private List<ListItem> mFilteredList;
 
-    public MyAdapter(List<ListItem> listItems, Context context) {
-        this.listItems = listItems;
+    private ArrayList<ListItem> mArrayList;
+    private List<ListItem> mFilteredList;
+
+//    public MyAdapter(List<ListItem> listItems, Context context, List<ListItem> mFilteredList) {
+//        this.listItems = listItems;
+//        this.context = context;
+//        this.listItems = mFilteredList;
+//    }
+
+    public MyAdapter(List<ListItem> arrayList, Context context) {
+        //this.listItems = listItems;
+        this.mArrayList  = (ArrayList<ListItem>) arrayList;
         this.context = context;
+        this.mFilteredList = arrayList;
+       // this.listItems = mFilteredList;
     }
+
+//    public MyAdapter(List<ListItem> listItems, Context applicationContext) {
+//    }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -37,7 +55,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        final ListItem listItem = listItems.get(position);
+        final ListItem listItem = mArrayList.get(position);
 
         holder.textViewHead.setText(listItem.getHead());
         holder.textViewDes.setText(listItem.getDesc());
@@ -47,18 +65,61 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         Picasso.with(context)
                 .load(listItem.getImageUrl())
                 .into(holder.imageView);
-        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(context, "clicked" + listItem.getHead(), Toast.LENGTH_LONG).show();
-            }
-        });
+//        holder.linearLayout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(context, "clicked" + listItem.getHead(), Toast.LENGTH_LONG).show();
+//            }
+//        });
     }
 
     @Override
     public int getItemCount() {
-        return listItems.size();
+        return mFilteredList.size();
     }
+
+    //Search Filter
+    @Override
+    public Filter getFilter() {
+
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+
+                String charString = charSequence.toString();
+
+                if (charString.isEmpty()) {
+
+
+                    mFilteredList = mArrayList;
+                } else {
+
+                    List<ListItem> filteredList = new ArrayList<>();
+
+                    for (ListItem listItem : mArrayList) {
+
+                        if (listItem.getHead().toLowerCase().contains(charString) || listItem.getDesc().toLowerCase().contains(charString) || listItem.getRef().toLowerCase().contains(charString)) {
+
+                            filteredList.add(listItem);
+                        }
+                    }
+
+                    mFilteredList = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = mFilteredList;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                mFilteredList = (ArrayList<ListItem>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
@@ -78,4 +139,6 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
             linearLayout = (LinearLayout) itemView.findViewById(R.id.linearLayout);
         }
     }
+
+
 }
